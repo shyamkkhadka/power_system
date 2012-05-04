fe1ab56af6487314a47c70070f9c083c97687a3b
Shyam Krishna Khadka <shyamkkhadka@gmail.com>
2012-05-04 15:29:48 +0545

    Uniqueness change of battery model no. changed

--- app/models/battery.rb
+++ app/models/battery.rb
@@ -1,6 +1,7 @@
 class Battery < ActiveRecord::Base
-	validates :model_no, :presence => true, :uniqueness => true
-  validates :cells_no , :numericality => true
+	validates :model_no, :presence => true
+        #:uniqueness => true
+        validates :cells_no , :numericality => true
 	
 	belongs_to :station
 end
